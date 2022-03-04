ba8c9c7650abe1233c14515957bd5fd4e6a0b536
lucky845 <lc1447545564@gmail.com>
2022-03-04 19:44:54 +0800

    优化了冗余代码

--- src/api/core/integral-grade.js
+++ src/api/core/integral-grade.js
@@ -1,25 +1,28 @@
 // @ 符号在vue.config.js 中配置， 表示 'src' 路径的别名
 import request from '@/utils/request'
 
+// 路径前缀
+const API = '/admin/core/integralGrade'
+
 export default {
   // 查询积分等级列表
   list() {
     return request({
-      url: '/admin/core/integralGrade/list',
+      url: `${API}/list`,
       method: 'get'
     })
   },
   // 根据id删除积分等级数据
   removeById(id) {
     return request({
-      url: `/admin/core/integralGrade/remove/${id}`,
+      url: `${API}/remove/${id}`,
       method: 'delete'
     })
   },
   // 新增数据
   save(integralGrade) {
     return request({
-      url: `/admin/core/integralGrade/save`,
+      url: `${API}/save`,
       method: 'post',
       data: integralGrade
     })
@@ -27,14 +30,14 @@ export default {
   // 根据id获取数据,用于修改界面数据回显
   getById(id) {
     return request({
-      url: `/admin/core/integralGrade/get/${id}`,
+      url: `${API}/get/${id}`,
       method: 'get'
     })
   },
   // 根据id修改对应的积分等级数据
   updateById(integralGrade) {
     return request({
-      url: `/admin/core/integralGrade/update`,
+      url: `${API}/update`,
       method: 'put',
       data: integralGrade
     })
