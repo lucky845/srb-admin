ccde5fc01739f0ac6c16fcb1e952c4e98f5ab5e1
lucky845 <lc1447545564@gmail.com>
2022-03-12 10:07:12 +0800

    标的管理功能

---
+++ src/api/core/lend.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+export default {
+  // 展示标的列表
+  getList() {
+    return request({
+      url: `/admin/core/lend/list`,
+      method: 'get'
+    })
+  },
+  // 标的详情
+  show(id) {
+    return request({
+      url: `/admin/core/lend/show/${id}`,
+      method: 'get'
+    })
+  }
+}
