35798674fee41c50cf43483494dfe1cd00da7c20
lucky845 <lc1447545564@gmail.com>
2022-03-14 20:53:43 +0800

    显示投资记录

--- src/views/core/lend/detail.vue
+++ src/views/core/lend/detail.vue
@@ -132,6 +132,24 @@
       </tbody>
     </table>
 
+    <h4>投资记录</h4>
+    <el-table :data="lendItemList" stripe style="width: 100%" border>
+      <el-table-column type="index" label="序号" width="70" align="center" />
+      <el-table-column prop="lendItemNo" label="投资编号" />
+      <el-table-column prop="investName" label="投资用户" />
+      <el-table-column prop="investAmount" label="投资金额" />
+      <el-table-column label="年化利率">
+        <template slot-scope="scope">
+          {{ scope.row.lendYearRate * 100 }}%
+        </template>
+      </el-table-column>
+      <el-table-column prop="investTime" label="投资时间" />
+      <el-table-column prop="lendStartDate" label="开始日期" />
+      <el-table-column prop="lendEndDate" label="结束日期" />
+      <el-table-column prop="expectAmount" label="预期收益" />
+      <el-table-column prop="investTime" label="投资时间" />
+    </el-table>
+
     <el-row style="text-align:center;margin-top: 40px;">
       <el-button @click="back">
         返回
@@ -143,6 +161,7 @@
 <script>
 import lendApi from '@/api/core/lend'
 import '@/styles/show.css'
+import lendItemApi from '@/api/core/lend-item'
 
 export default {
   data() {
@@ -152,23 +171,33 @@ export default {
           param: {}
         },
         borrower: {}
-      }
+      },
+      lendItemList: [] // 投资列表
     }
   },
 
   created() {
     if (this.$route.params.id) {
       this.fetchDataById()
+      // 投资记录
+      this.fetchLendItemList()
     }
   },
 
   methods: {
+    // 根据id查询数据
     fetchDataById() {
       lendApi.show(this.$route.params.id).then(response => {
         this.lendDetail = response.data.lendDetail
       })
     },
-
+    // 获取投资列表
+    fetchLendItemList() {
+      lendItemApi.getList(this.$route.params.id).then(response => {
+        this.lendItemList = response.data.list
+      })
+    },
+    // 返回按钮
     back() {
       this.$router.push({ path: '/core/lend/list' })
     }
